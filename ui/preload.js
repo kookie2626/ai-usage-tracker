a52f0e879758a5a680ae1381ee865f2fc5691a7d
kookie2626 <keunlee726@gmail.com>
2026-05-02 13:10:29 +0900

    Initial commit: AI Usage Tracker (macOS Menu Bar)

---
+++ ui/preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    getUsageData: () => ipcRenderer.invoke('get-usage-data'),
+    loadKeys: () => ipcRenderer.invoke('load-keys'),
+    saveKeys: (keys) => ipcRenderer.invoke('save-keys', keys),
+    authenticateProvider: (provider, alias) => ipcRenderer.invoke('authenticate-provider', provider, alias),
+    loadBudget: () => ipcRenderer.invoke('load-budget'),
+    saveBudget: (budget) => ipcRenderer.invoke('save-budget', budget),
+    resizeWindow: (width, height) => ipcRenderer.invoke('resize-window', width, height),
+    hideApp: () => ipcRenderer.invoke('hide-app'),
+    quitApp: () => ipcRenderer.invoke('quit-app')
+});
